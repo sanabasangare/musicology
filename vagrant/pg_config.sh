49e6b183e5cc504276404136e644ca759d899cd5
Sanaba Sangare <sanabasangare@users.noreply.github.com>
2016-09-06 18:37:30 -0400

    chore: Create pg_config.sh

---
+++ vagrant/pg_config.sh
@@ -0,0 +1,12 @@
+apt-get -qqy update
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+pip install bleach
+pip install oauth2client
+pip install requests
+pip install httplib2
+pip install redis
+pip install passlib
+pip install itsdangerous
+pip install flask-httpauth
